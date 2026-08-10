a81bd5d4c9e8b3687cc74a6eabbbdac6cb610177
Aashique Karn <aashiquekarn@gmail.com>
2026-08-11 00:21:51 +0530

    main | added probability distribution example.

--- probability_distribution.py
+++ probability_distribution.py
@@ -9,3 +9,20 @@ plt.plot(x, stats.norm.pdf(x, loc=0, scale=1))
 plt.title('Normal Distribution')
 plt.savefig('normal_distribution.png')
 plt.show()
+
+np.random.seed(42)
+population = 100_000
+has_disease = np.random.rand(population) < 0.001
+test_positive = np.where(
+    has_disease,
+    np.random.rand(population) < 0.99,
+    np.random.rand(population) < 0.01,
+)
+probability_disease_given_positive = has_disease[test_positive].mean()
+print("Bayes theorem:-", probability_disease_given_positive)
+
+data = np.array([2, 4, 4, 4, 5, 5, 7, 9])
+print("Mean:-", np.mean(data))
+print("Median:-", np.median(data))
+print("Standard Deviation:-", np.std(data))
+print("Variance:-", np.var(data))
