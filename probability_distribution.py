1d1eb566c1670e29981ec54b2ae49712d6b8c1d7
Aashique Karn <aashiquekarn@gmail.com>
2026-08-11 11:03:10 +0530

    main | added probability distribution p-values, hypothesis testing examples.

--- probability_distribution.py
+++ probability_distribution.py
@@ -8,7 +8,7 @@ x = np.linspace(-4, 4, 200)
 plt.plot(x, stats.norm.pdf(x, loc=0, scale=1))
 plt.title('Normal Distribution')
 plt.savefig('normal_distribution.png')
-plt.show()
+# plt.show()
 
 np.random.seed(42)
 population = 100_000
@@ -26,3 +26,18 @@ print("Mean:-", np.mean(data))
 print("Median:-", np.median(data))
 print("Standard Deviation:-", np.std(data))
 print("Variance:-", np.var(data))
+
+
+np.random.seed(1)
+group_a = np.random.binomial(1, 0.05, 200)
+group_b = np.random.binomial(1, 0.07, 200)
+t_stat, p_value = stats.ttest_ind(group_a, group_b)
+print(f"Group A conversion : {group_a.mean():.3f}")
+print(f"Group B conversion : {group_b.mean():.3f}")
+print(f"P-value : {p_value:.3f}")
+
+data_1 = np.random.normal(50, 10, 30)
+mean = np.mean(data_1)
+sem = stats.sem(data_1) # standard error of the mean
+ci = stats.t.interval(0.95, df=len(data_1)-1, loc=mean, scale=sem)
+print(f"Mean : {mean:.3f}, 95% CI : {ci}")
